208cb6db3ea9cb7491a7b8fc6c8bb247f6addff5
stevens456787 <stevenstrudient@gmail.com>
2025-02-01 13:13:33 +0300

    update the code files

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,17 +3,15 @@ from mysql.connector import Error
 
 def create_database():
     try:
-        # Connect to MySQL server
         connection = mysql.connector.connect(
-            host="localhost",  # Replace with your host
-            user="root",       # Replace with your username
-            password="Ronaldo@254"  # Replace with your password
+            host="localhost",
+            user="root",
+            password="Ronaldo@254"
         )
-
         if connection.is_connected():
             cursor = connection.cursor()
-            # Create database
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            connection.commit()  # Apply changes
             print("Database 'alx_book_store' created successfully!")
             cursor.close()
     except Error as e:
@@ -21,7 +19,7 @@ def create_database():
     finally:
         if connection.is_connected():
             connection.close()
-            print("Connection to MySQL closed.")
+            print("Connection closed.")
 
 if __name__ == "__main__":
     create_database()
